91bd2fee48c50c68e3443cfbfb7e0fdf0a1a04d8
Vejmon <89570452+Vejmon@users.noreply.github.com>
2023-02-02 13:02:59 +0100

    setter tilbake 'andreTall' fjerner en loop fra task4 ferdigstiller prosjektet

--- task4.py
+++ task4.py
@@ -1,37 +1,38 @@
 #gjenbruker funksjonen fra task2 igjen.
 from task2 import jainsall
+import sys
+
+#lager feilmelding hvis tekstfilen er i feil format.
+#som stopper resten av skriptet fra å kjøre
+def fantFeil():
+    print('Feil i listen; listen må ha dette oppsettet')
+    print('eksempel')
+    print('32 Gbps')
+    sys.exit('stopper script')
 
 #skal lese av en liste med tall og enheter, og sende disse tallene som om de var gitt i Mbps.
 with open('andreTall') as tekstFil:
-    #lager to lister, den ene skal holde verdien per linje, den andre enheten til verdien.
+    #lager listen over tall vi skal sende til jainsall.
     tall = []
-    enhet = []
 
-    #looper igjennom listen, og gir tallverdien riktig mengde utifra enheten.
+    #looper igjennom listen, og setter tallverdi i en liste i riktig størrelse i en liste.
+    #hvis textfilen ikke er i riktig format printes feilmeldingen med noen instruksjoner.
     for line in tekstFil:
+        #leser av linje for linje i tekstfilen.
         try:
-            tall.append(line.split()[0])
-            enhet.append(line.split()[1])
-        except:
-            print('listen må ha dette oppsettet')
-            print('eksempel')
-            print('32 Gbps')
-    #oppretter en teller for å hente riktig linje.
-    teller = 0
-    for i in tall:
-        #hvis textfilen ikke er i riktig format printes feilmeldingen med noen instruksjoner.
-        try:
-            i = float(i)
+            #sjekker om første del av listen er et tall.
+            etTall = float(line.split()[0])
+            enEnhet = line.split()[1]
 
             #konverterer tallverdier som ikke er gitt som Mbps
-            if enhet[teller][0] == 'K':
-                i = i / 1024
-            elif enhet[teller][0] == 'G':
-                i = i * 1024
+            if enEnhet[0] == 'K':
+                etTall = etTall / 1024
+            elif enEnhet[0] == 'G':
+                etTall = etTall * 1024
+
+            tall.append(etTall)
         except:
-            print('listen må ha dette oppsettet')
-            print('eksempel')
-            print('32 Gbps')
+            fantFeil()
 
     #sender listen til jainsall fra task2
     print(jainsall(tall))
